652016db1a20788143a01f4e2097619b0a6cfc25
Marcel Hauf <marcel.hauf@googlemail.com>
2011-08-19 13:06:59 +0200

    Add events. Will maybe used....

---
+++ events.go
@@ -0,0 +1,25 @@
+package baukasten
+
+const (
+	SystemEvent_Quit = iota
+)
+
+type FrameEvent struct {
+	DeltaTime uint
+	Ticks     uint32
+}
+
+type SystemEvent int
+
+type MouseMotionEvent interface {
+	Position() (int, int)
+	Difference() (int, int)
+}
+
+type KeyboardEvent interface {
+	// TODO Keyboard
+}
+
+type MouseButtonEvent interface {
+	// TODO MouseButton
+}
