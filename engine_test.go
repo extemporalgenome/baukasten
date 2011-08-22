6073808d94eb40e8bff7f1464c368fb51870b9b7
Marcel Hauf <marcel.hauf@googlemail.com>
2011-08-22 14:00:08 +0200

    Basic testing of the engine.

---
+++ engine_test.go
@@ -0,0 +1,32 @@
+package baukasten
+
+import (
+	"testing"
+	"os"
+)
+
+func TestEngine(t *testing.T) {
+	var err os.Error
+	graphicSettings := NewGraphicSettings(640, 480, 32, false, true, "Baukasten testing")
+	engine := NewEngine()
+	err = engine.Init(graphicSettings)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = engine.Resize(1024, 768)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for i := 0; i < 1000; i++ { // Do 1000 frames
+		engine.BeginFrame()
+		// TODO Drawing goes in here
+		engine.EndFrame()
+	}
+
+	err = engine.Quit()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
