91728a2cc18070c4261167319fab1d8540a8af3d
Marcel Hauf <marcel.hauf@googlemail.com>
2011-08-25 13:40:07 +0200

    Extend Colors.

--- color.go
+++ color.go
@@ -1,7 +1,23 @@
 package baukasten
 
+type Color3b struct {
+	R, G, B byte
+}
+
+func (c *Color3b) To3f() *Color3f {
+	return &Color3f{255 / c.R, 255 / c.G, 255 / c.B}
+}
+
 type Color3f struct {
 	R, G, B float32
+}
+
+type Color4b struct {
+	R, G, B, A byte
+}
+
+func (c *Color4b) To4f() *Color4f {
+	return &Color4f{255 / c.R, 255 / c.G, 255 / c.B, 255 / c.A}
 }
 
 type Color4f struct {
