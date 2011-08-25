cbce0ba3d19d5408eb0fe85084a7c7315478db8b
Marcel Hauf <marcel.hauf@googlemail.com>
2011-08-25 13:47:38 +0200

    Fix some conversion errors. Add standard color as Color struct, this struct will be used as the default color holder for all the library.

--- color.go
+++ color.go
@@ -5,7 +5,7 @@ type Color3b struct {
 }
 
 func (c *Color3b) To3f() *Color3f {
-	return &Color3f{255 / c.R, 255 / c.G, 255 / c.B}
+	return &Color3f{255 / float32(c.R), 255 / float32(c.G), 255 / float32(c.B)}
 }
 
 type Color3f struct {
@@ -17,9 +17,21 @@ type Color4b struct {
 }
 
 func (c *Color4b) To4f() *Color4f {
-	return &Color4f{255 / c.R, 255 / c.G, 255 / c.B, 255 / c.A}
+	return &Color4f{255 / float32(c.R), 255 / float32(c.G), 255 / float32(c.B), 255 / float32(c.A)}
 }
 
 type Color4f struct {
 	R, G, B, A float32
+}
+
+type Color struct {
+	R, G, B, A byte
+}
+
+func NewColor(r, g, b, a byte) *Color {
+	return &Color{r, g, b, a}
+}
+
+func NewColorF(r, g, b, a float32) *Color {
+	return &Color{byte(255 * r), byte(255 * g), byte(255 * b), byte(255 * a)}
 }
