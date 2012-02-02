4fb3acd95e31fc5b3555f7e34ac999340b73200c
Marcel Hauf <marcel.hauf@googlemail.com>
2012-02-02 18:54:11 +0100

    Add Pi function

--- math.go
+++ math.go
@@ -4,32 +4,37 @@ import (
 	"math"
 )
 
-// Similar to math.Acos but takes and returns a float32 instead of a float64 
+// Pi returns the math.Pi number as a float32.
+func Pi() float32 {
+	return math.Pi
+}
+
+// Similar to math.Acos but takes and returns a float32 instead of a float64.
 func Acos(angle float32) float32 {
 	return float32(math.Acos(float64(angle)))
 }
 
-// Similar to math.Cos but takes and returns a float32 instead of a float64 
+// Similar to math.Cos but takes and returns a float32 instead of a float64.
 func Cos(angle float32) float32 {
 	return float32(math.Cos(float64(angle)))
 }
 
-// Similar to math.Sin but takes and returns a float32 instead of a float64 
+// Similar to math.Sin but takes and returns a float32 instead of a float64.
 func Sin(angle float32) float32 {
 	return float32(math.Sin(float64(angle)))
 }
 
-// Similar to math.Sqrt but takes and returns a float32 instead of a float64 
+// Similar to math.Sqrt but takes and returns a float32 instead of a float64.
 func Sqrt(value float32) float32 {
 	return float32(math.Sqrt(float64(value)))
 }
 
-// Similar to math.Signbit but takes and returns a float32 instead of a float64 
+// Similar to math.Signbit but takes and returns a float32 instead of a float64.
 func Signbit(value float32) bool {
 	return math.Signbit(float64(value))
 }
 
-// Similar to math.Tan but takes and returns a float32 instead of a float64 
+// Similar to math.Tan but takes and returns a float32 instead of a float64.
 func Tan(x float32) float32 {
 	return float32(math.Tan(float64(x)))
 }
