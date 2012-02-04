5f742cebc5d193bc72a1ee4888fe42cd1ceaa3f2
Marcel Hauf <marcel.hauf@googlemail.com>
2012-02-04 18:28:20 +0100

    particles:Add GravityPointManipulator

---
+++ particles/gravityPointManipulator.go
@@ -0,0 +1,37 @@
+package particles
+
+import (
+	"time"
+
+	"github.com/Agon/baukasten"
+)
+
+type GravityPointManipulator struct {
+	Position baukasten.Vector2
+	Force    float32
+	Range    float32
+}
+
+func NewGravityPointManipulator(position baukasten.Vector2, force, gravityRange float32) *GravityPointManipulator {
+	return &GravityPointManipulator{Position: position, Force: force, Range: gravityRange}
+}
+
+func (m *GravityPointManipulator) Update(deltaTime time.Duration, particles []Particle) {
+	if m.Range < 0 { // Global effect
+		for i := range particles {
+			way := particles[i].Position.Sub(m.Position)
+			way.Normalize()
+			way.Scale(m.Force)
+			particles[i].Velocity.Accumulate(way)
+		}
+		return
+	}
+	for i := range particles {
+		if particles[i].Position.DistanceBetween(m.Position) <= m.Range {
+			way := particles[i].Position.Sub(m.Position)
+			way.Normalize()
+			way.Scale(m.Force)
+			particles[i].Velocity.Accumulate(way)
+		}
+	}
+}
