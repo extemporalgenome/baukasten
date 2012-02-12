9883219e6fcdec23487b392b0fbb506f6bee4fad
Marcel Hauf <marcel.hauf@googlemail.com>
2012-02-12 18:33:00 +0100

    ogl: Add Uniform4f function

--- driver/ogl/uniformLocation.go
+++ driver/ogl/uniformLocation.go
@@ -12,6 +12,10 @@ func (loc *UniformLocation) Uniform1i(value int) {
 	gl.Uniform1i(loc.id, gl.Int(value))
 }
 
+func (loc *UniformLocation) Uniform4f(v0, v1, v2, v3 float32) {
+	gl.Uniform4f(loc.id, gl.Float(v0), gl.Float(v1), gl.Float(v2), gl.Float(v3))
+}
+
 func (loc *UniformLocation) UniformMatrix4fv(count int, transpose bool, matrix []float32) {
 	gl.UniformMatrix4fv(loc.id, gl.Sizei(count), gl.GLBool(transpose), (*gl.Float)(&matrix[0]))
 }
